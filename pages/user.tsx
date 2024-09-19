a04d8f6dbf3356755a1df34ed8c12fc47521e534
Santiago Barchetta <dudesl@gmail.com>
2024-09-19 15:19:27 -0300

    title pages

--- pages/user.tsx
+++ pages/user.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Github User</title>
-        <meta name="description" content="Just another Github user explorer" />
+        <title>yet another github user search</title>
+        <meta name="description" content="yet another github user search" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
